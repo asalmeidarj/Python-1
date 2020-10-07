4c7aa0cefa6dcd54e98306a23009214f65510bdc
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-10-07 11:06:33 -0300

    exemploClass

--- class/classe01.py
+++ class/classe01.py
@@ -5,22 +5,22 @@ class Livro:
     # Este método vai inicializar cada objeto criado a partir desta classe
     # O nome deste método é __init__
     # (self) é uma ref. a cada atributo de um obj. criado a partir desta classe
-    def __init__(self):  # Construtor
+    def __init__(self):  # Inicializador
         self.titulo = "O monge e o Executivo"
         self.isbn = 9988888
-        print("Construtor chamado para criar um objeto desta classe")
+        print("Inicializador chamado para criar um objeto desta classe")
 
     def imprime(self):  # Método recebe como parâmetro atributos do obj. criado
         print("Foi criado o livro %s e ISBN %d" % (self.titulo, self.isbn))
 
 
 #########################################
-#  Comandos para utilizar no terminal   #
 # Criando uma instância da classe livro #
+#  *Comandos para utilizar no terminal* #
 #         ipython -i classes01.py       #
 #         Livro1 = Livro()              #
 #         type(Livro1)                  #
 #         Livro1.titulo                 #
-#         Livro1.ISBN                   #
+#         Livro1.isbn                   #
 #         Livro1.imprime                #
 #########################################
