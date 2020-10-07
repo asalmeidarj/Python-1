9a8eb86ac2d90f5200d7666354528ad5ddc38714
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-10-07 11:08:03 -0300

    Exemplos de criação de Classes

---
+++ class/classes02.py
@@ -0,0 +1,31 @@
+""" Classe de criação de fila"""
+
+
+class Fila:
+    def __init__(self):  # Método inicializador
+        self.fila = []
+        print("Inicializador chamado para criar um objeto desta classe")
+
+    def entrar(self, nome):  # Função que informa quem entra na fila.
+        self.fila.append(nome)  # Utilizado para adicionar nomes na fila.
+        print(f"{nome} entrou na fila")
+
+    def sair(self):  # Função que informa quem sai da fila.
+        self.fila.pop(0)  # Utilizado para retirar nomes da fila.
+        print("Alguém saiu da fila")
+
+
+#########################################
+# Criando uma instância da classe livro #
+#  *Comandos para utilizar no terminal* #
+#         ipython -i classes02.py       #
+#         Banco = Fila()                #
+#         type(Banco)                   #
+#         Banco.entrar('Daniel')        #
+#         Banco.entrar('Alessandro')    #
+#         Banco.fila                    #
+#         Banco.sair ( )                #
+#         Banco.fila                    #
+#         Banco.sair ( )                #
+#         Banco.fila                    #
+#########################################
