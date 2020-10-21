80de1578e4b5f5ad1df1ada1b98f43be572f8e05
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-10-20 23:31:53 -0300

    Update arredondamento.py

--- matemática/arredondamento.py
+++ matemática/arredondamento.py
@@ -1,1 +1,16 @@
+""" Arredondar número utilizando função intera round """                  
 
+
+def arredondar_valor():
+    primeiro_num = 1.23456789  # número aleatório 
+    segundo_num = 3  # quantidade de algarismos para arredondar
+    print(round(primeiro_num, segundo_num)) 
+
+
+arredondar_valor() 
+
+##########################################################
+# Quando um valor está exatamente entre                  #
+# duas opções, o comportamento de arredondamento         #
+# consiste em arredondar para o dígito par mais próximo. #
+##########################################################
