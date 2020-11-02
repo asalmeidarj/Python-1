dd6f6065718df4aa52e9881c3be7dea1400ee147
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-02 18:40:57 -0300

    if elif else

---
+++ 02_Decisão_e_Condição/delta_04.py
@@ -0,0 +1,22 @@
+""" Raízes de uma equação de segundo grau """
+
+import math
+
+
+def delta():
+    a = int(input("Digite o valor do coeficiente a: "))
+    b = int(input("Digite o valor do coeficiente b: "))
+    c = int(input("Digite o valor do coeficiente c: "))
+    delta = b*b - 4 * a * c
+    if delta < 0:
+        print("A equação não possui raízes reais.")
+    elif delta == 0:
+        raiz = (-1*b + math.sqrt(delta)) / (2 * a)
+        print(f'A equação possui apenas uma raíz: {raiz}')
+    elif delta > 0:
+        raiz_1 = (-1*b - math.sqrt(delta)) / (2 * a)
+        raiz_2 = (-1*b + math.sqrt(delta)) / (2 * a)
+        print(f'As raízes da equação são: {raiz_1} e: {raiz_2}')
+
+
+delta()
