ce2030e9b60075d76ac75a0c00d2133083e91f7f
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-02 19:03:25 -0300

    Contador

---
+++ 04_Repetição While/contador_01.py
@@ -0,0 +1,11 @@
+""" Contador até 99 """
+
+
+def contador():
+    contador = 0  # Contador inicia aqui
+    while contador < 100:  # contador termina no número 99
+        print(contador)
+        contador += 1
+
+
+contador()
