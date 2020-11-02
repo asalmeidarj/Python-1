bf4a307b022a0e4b71b0b2569c3ad310356d56bf
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-02 13:17:48 -0300

    Decisão e condição

---
+++ 02_Decisão_e_Condição/idade_01.py
@@ -0,0 +1,12 @@
+""" Verificar se é maior ou menor de idade """
+
+
+def idade():
+    x = int(input('idade: '))
+    if x >= 18:
+        print('maior de idade')
+    else:
+        print('menor de idade')
+
+
+idade()
