18003f7ddb20516b877d5835b1078818dbc6e182
Alessandro Almeida <43619109+asalmeidarj@users.noreply.github.com>
2020-11-03 14:01:19 -0300

    Update delta_04.py

--- 02_Decisão_e_Condição/delta_04.py
+++ 02_Decisão_e_Condição/delta_04.py
@@ -2,21 +2,26 @@
 
 import math
 
+def vertice(delta, a, b):
+    xv = -b/(2*a)
+    yv = -delta/(4*a)
+    return (xv, yv)
 
 def delta():
     a = int(input("Digite o valor do coeficiente a: "))
     b = int(input("Digite o valor do coeficiente b: "))
     c = int(input("Digite o valor do coeficiente c: "))
     delta = b*b - 4 * a * c
+    xv, yv = vertice(delta, a, b)
     if delta < 0:
-        print("A equação não possui raízes reais.")
+        print("A equação não possui raízes reais.")      
     elif delta == 0:
         raiz = (-1*b + math.sqrt(delta)) / (2 * a)
         print(f'A equação possui apenas uma raíz: {raiz}')
     elif delta > 0:
         raiz_1 = (-1*b - math.sqrt(delta)) / (2 * a)
         raiz_2 = (-1*b + math.sqrt(delta)) / (2 * a)
-        print(f'As raízes da equação são: {raiz_1} e: {raiz_2}')
-
+        print(f'As raízes da equação são: {raiz_1} e: {raiz_2}')   
+    print(f"O vértice da parábola {a}x^2 + {b}x + {c} : ({xv}, {yv})")
 
 delta()
