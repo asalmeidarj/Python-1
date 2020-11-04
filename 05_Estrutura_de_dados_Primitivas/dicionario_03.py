aecb86c1e479db77bcf003d5261ce75c705e0ec4
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-04 10:29:30 -0300

    tupla

---
+++ 05_Estrutura_de_dados_Primitivas/dicionario_03.py
@@ -0,0 +1,15 @@
+""" Exemplo de Dicionário """
+
+
+def dicionario():
+    paises = {'BR': 'Brasil', 'EUA': 'Estados Unidos',
+              'JP': 'Japão', 'CN': 'China'}
+    print(paises)
+    print(paises['BR'])
+    print(paises['EUA'])
+    print(paises['JP'])
+    print(paises['CN'])
+    print(type(paises))
+
+
+dicionario()
