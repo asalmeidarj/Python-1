1b65d32e384f6531415ccc23432a33b05eeb53be
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-04 10:58:44 -0300

    pilha

---
+++ 06__Estrutura_de_dados_Derivadas/pilhas-01.py
@@ -0,0 +1,20 @@
+""" Pilhas """
+
+
+def pilha():
+    pilha = [2, 4, 6, 8]
+    pilha.append(10)  # append adiciona elemento acima da pilha
+    pilha.append(20)
+    pilha.append(40)
+    pilha.append(80)
+    pilha.append(160)
+    pilha.append(320)
+
+    print(pilha)
+    pilha.pop()  # pop remove o último elemento adiciona na pilha
+    print(pilha)
+    pilha.pop()  # pop remove o último elemento adiciona na pilha
+    print(pilha)
+
+
+pilha()
